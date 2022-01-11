92e328ff2fd2a0a5fc74da3e90d5c3592a968eff
dom <kwabina.adomako@yahoo.com>
2022-01-11 15:53:37 -0800

    new changes to exams report

--- student_exams_report/examination_report.py
+++ student_exams_report/examination_report.py
@@ -1,4 +1,5 @@
 from grading_system import school_courses, general_subjects
+import datetime as dt
 
 grading_list = []
 
@@ -51,11 +52,9 @@ def grading():
 
 
 school_courses()
-print()
 
 
 select_course = int(input('Please select course using course number:> '))
-print()
 
 # fetch the values(subjects) from the dict based on selected key(Course)
 display_select_course = school_course[select_course]
@@ -65,7 +64,6 @@ for i, subjects in enumerate(display_select_course, start=1):
     print(i, subjects)
 
 # adding selected subjects to list
-print()
 while True:
     add_subjects = int(input('Add subjects by selecting subject number:> '))
     adding_course = display_select_course[add_subjects - 1]
@@ -88,8 +86,8 @@ while True:
         break
 
 if length_of_marks_list == length_of_subject_list:
-    print()
+    print(f'Terminal Report for {dt.date.year} Academic Year')
     for subjects, student_marks, grades in zip(subject_list, subject_marks, grading_list):
         print('{}----------{}--({})'.format(subjects, student_marks, grades))
-    print()
     print('Examination report successfully generated!!!')
+print(f'Report printed on {dt.datetime.today()}')
